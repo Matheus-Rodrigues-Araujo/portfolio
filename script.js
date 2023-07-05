bc264614d7f48e782990243a94539cc7a5e46f0e
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-07-05 17:36:57 -0300

    NEW: Background video has been included and optimized

---
+++ script.js
@@ -0,0 +1,16 @@
+var video = document.getElementById('video-background');
+
+// Reinicia o vídeo quando termina
+video.addEventListener('ended', function() {
+    video.currentTime = 0;
+    video.play();
+});
+
+// Pausa o vídeo quando o usuário sai da visualização
+window.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+    video.pause();
+    } else {
+    video.play();
+    }
+});
