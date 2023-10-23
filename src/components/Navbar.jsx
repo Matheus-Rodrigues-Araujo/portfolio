ff4203e3222b81aa262cad4e1e9c64ae95e71b56
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-10-23 17:06:55 -0300

    design updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo_official.png'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark fixed-top">
-            
+            saddddddddd
             </nav>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,28 @@
 import '../index.css'
+import draw from '../assets/images/draw.jpg'
+
 const Home = () =>{
     return (
-            <section className="main"  style={{minHeight: '100vh'}} > 
-              <div className="text-uppercase bg-red-400" > </div>
-              <div className="bg-blue"></div> 
-            </section>
+      <main className='' >
+
+        <section className="relative flex justify-center items-start  bg-red-500 h-screen " >
+          <div className='z-0 absolute grid grid-cols-2 h-100' >
+            <div className='' style={{backgroundColor: '#110927', width: '50vw'}} >
+              <h1 style={{color: '#FB3137'}} >Front-end Developer.</h1>
+            </div>
+            <div style={{backgroundColor: '#FB3137', width: '50vw'}} ></div>
+          </div>
+
+          <div className='absolute personal-img flex justify-center items-center mt-60  rounded-full bg-white' >
+              <img src={draw} className='' height={200} width={200} alt="Personal drawn"/>
+          </div>
+        
+        </section>
+        <section className="grid grid-cols-2 bg-white h-screen" > 
+                <div className=''></div>
+                <div className=''></div>
+        </section>
+      </main>
     )
 }
 
