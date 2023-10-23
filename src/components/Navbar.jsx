7c599c854755b9538cb07522ff582015c3404c5d
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-10-23 20:36:10 -0300

    design ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,17 @@ import logo from '../assets/images/logo_official.png'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark fixed-top">
-            saddddddddd
-            </nav>
+        <nav className="flex justify-between items-center navbar fixed-top w-100 px-64  ">
+            <Link to={'/'} className='active'>
+                <img src={logo} height={50} width={50}  alt="" />
+            </Link>
+            <div className='flex gap-32 justify-center uppercase' >
+                <Link to="/" >Home</Link> 
+                <Link to="/" >About</Link>
+                <Link to="/">Projects</Link>
+                <Link to="/">Contact</Link>
+            </div>
+        </nav>
     )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,22 +1,30 @@
 import '../index.css'
+import './../styles/App.scss'
 import draw from '../assets/images/draw.jpg'
 
 const Home = () =>{
     return (
       <main className='' >
 
-        <section className="relative flex justify-center items-start  bg-red-500 h-screen " >
-          <div className='z-0 absolute grid grid-cols-2 h-100' >
-            <div className='' style={{backgroundColor: '#110927', width: '50vw'}} >
-              <h1 style={{color: '#FB3137'}} >Front-end Developer.</h1>
+        <section className="h-screen" >
+          <div className='grid grid-cols-2 h-100 ' >
+            <div className='relative flex items-start justify-start flex-column justify-start px-72' style={{backgroundColor: '#110927', width: '50vw'}} >
+              <h1 className='text-[10rem] font-bold mt-80 pt-40 text-left'  style={{width:'50%', color: '#FB3137'}} >Front-end Developer.</h1>
+              <p className='text-white text-[4rem] font-light text-left' >"I'm deeply passionate about meticulously crafting visually stunning and highly functional websites that leave a lasting impression."</p>
+            
+              <div className='comments absolute flex gap-5 bottom-20 left-0 px-64' >
+                <p className='text-[3.5rem] font-light text-left' >I like to design and build beautiful websites</p>
+                <p className='text-[3.5rem] font-light text-left' >I like to design and build beautiful websites</p>
+              </div>
             </div>
-            <div style={{backgroundColor: '#FB3137', width: '50vw'}} ></div>
+
+            <div className='flex justify-end pt-20 px-64' style={{backgroundColor: '#FB3137', width: '50vw'}}>
+              <div className=' personal-img flex justify-center items-center mt-80  rounded-full bg-white' >
+                <img src={draw} className='' height={250} width={250} alt="Personal drawn"/>
+            </div>
+            </div>
           </div>
 
-          <div className='absolute personal-img flex justify-center items-center mt-60  rounded-full bg-white' >
-              <img src={draw} className='' height={200} width={200} alt="Personal drawn"/>
-          </div>
-        
         </section>
         <section className="grid grid-cols-2 bg-white h-screen" > 
                 <div className=''></div>
