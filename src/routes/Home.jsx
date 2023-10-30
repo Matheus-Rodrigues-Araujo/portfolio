2110519b1bbcab631f7bda1291ee2e6e3ce469a5
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-10-30 00:04:58 -0300

    ok

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -26,7 +26,7 @@ const Home = () =>{
         };
     }, []);
     return (
-      <main className='sm:top-0 h-screen mt-[35px]  ml-[0px] md:pt-[0px] mt-0 ml-40' >
+      <main className={`${isMobile ? 'sm:top-0 h-screen mt-[35px] ml-[0px]' :'pt-[0px] mt-0 ml-40'}`} >
 
         <section id='main' className={`main-section ${isMobile ? 'top-0 pt-40': 'h-screen pt-0'}`} >
           {/* BRAND + NAME */}
@@ -39,28 +39,25 @@ const Home = () =>{
           <div className={`${
             isMobile ? 
             'px-20 grid grid-cols-1 grid-rows-2 gap-0 mt-14': 
-            'px-64 grid grid-cols-2 grid-rows-2 gap-0'
+            'relative px-64 gap-0 flex flex-column justify-start h-100'
             }`} >
-              <div>
+              <div className={`${!isMobile && 'flex flex-column gap-12'}`} >
                 <h1 className={`${ 
                   isMobile ? 
                   'text-[6rem] font-bold text-left w-100': 
-                  'text-[8rem] font-bold text-left '
+                  'text-[10rem] font-bold text-left mt-10'
                   }`} style={{color: '#FB3137'}} >Front-end Developer.</h1>
                 <p className={`${ 
                   isMobile ? 
                   'text-white font-bold font-light text-left text-[2rem]' : 
-                  'text-white font-bold font-light text-left text-[3.5rem]'}`} >
+                  'text-white font-bold font-light text-left text-[4rem] w-50'}`} >
                     I'm deeply passionate about meticulously crafting visually stunning and highly functional websites that leave a lasting impression.
                 </p>
               </div>
 
-              {/* Greetings image */}
-              <div className={`${isMobile ? 'hidden' :'absolute right-80 top-60 rounded-full'}`} >
-                <img src={draw} className="" alt="Personal drawn"/>
-              </div> 
               
-              <div className={`comments ${isMobile ? 'flex gap-5 bottom-20 self-center w-75': 'absolute bottom-20 left-60 w-50'}`} >
+              
+              <div className={`comments ${isMobile ? 'flex gap-5 bottom-20 self-center w-75': 'mt-12'}`} >
                 <p className='text-[2.5rem] font-light md:text-[3.5rem]' >Solid problem-solving and code debugging skills to create effective and efficient solutions.</p>
               </div>
 
@@ -68,7 +65,7 @@ const Home = () =>{
               <div className={`${
                 isMobile ? 
                 'links flex justify-start gap-5 py-5 w-100 ml-0' :
-                'absolute bottom-20 right-[80px] links flex justify-center '}`}
+                'links flex my-10'}`}
               >
                 <a href="https://www.linkedin.com/in/matheus-rodrigues-araujo/" target='_blank'  >
                   <AiFillLinkedin className='w-[50px] h-[45px]' />
@@ -81,10 +78,11 @@ const Home = () =>{
                 </a>
               </div>
           </div>
+          
           {/* Image for mobile devices */}
-          <div className='w-100 bg-red-500 p-5 w-screen md:hidden' >
-          <img src={draw} className="h-[30%] w-[30%] rounded-md mx-auto bg-white p-5 rounded-[4em] md:h-[220px] w-[220px]" alt="Personal drawn"/>
-          </div> 
+          {/* <div className='w-100 bg-red-500 p-5 w-screen md:hidden' >
+            <img src={draw} className="h-[30%] w-[30%] rounded-md mx-auto bg-white p-5 rounded-[4em] md:h-[220px] w-[220px]" alt="Personal drawn"/>
+          </div>  */}
         </section>
 
         {/*About section  */}
