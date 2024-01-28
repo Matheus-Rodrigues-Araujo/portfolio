85d7e87a778353b954442e2b12493b59e867d4f4
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-01-28 02:20:10 -0300

    saved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,69 @@
-import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/images/logo_official.png'
-import { AiFillHome, AiFillBook, AiFillPhone, AiFillMail } from 'react-icons/ai'
-import { BsBriefcaseFill } from 'react-icons/bs'
-import { useState, useEffect } from 'react'
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/images/logo_official.png";
+import {
+  AiFillHome,
+  AiFillBook,
+  AiFillPhone,
+  AiFillMail,
+} from "react-icons/ai";
+import { BsBriefcaseFill } from "react-icons/bs";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
-    const [isMobile, setIsMobile] = useState(false);
+  return (
+    <nav
+      className={`navbar flex bg-lightYellow h-64 w-full fixed-top items-center px-40`}
+    >
+      <Link to={"/"} className="text-[8rem]">
+        Matheus
+      </Link>
 
-    useEffect(() => {
-        const handleResize = () => {
-        setIsMobile(window.innerWidth <= 768); // Define o breakpoint para "md" como 768 pixels
-        };
-
-        window.addEventListener('resize', handleResize);
-        handleResize();
-
-        return () => {
-        window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    return (
-        <nav 
-        className={
-            `navbar bg-white ${isMobile ?
-                 'flex h-auto fixed-top flex-row items-center px-[40px]' :
-                 'flex fixed-top flex-column w-[80px] gap-32 py-20 justify-start h-screen'}`}
+      <div className="nav-links flex justify-between items-center mt-0 gap-[20px]">
+        <a
+          className="text-[4rem] font-[260] bg-normalBlue text-black hover:underline"
+          href="#main"
+          title="Home"
         >
-            <Link to={'/'} className='active'> <img src={logo} className='logo' alt="logo"/></Link>
-            
-            <div className={`nav-links ${isMobile ? 'flex justify-between items-center mt-0 gap-[20px]':'grid gap-32 justify-center'}`}>
-                <a href="#main" title='Home' >
-                    <AiFillHome/>
-                </a> 
-                <a href="#about" title="About">
-                    <AiFillBook/>
-                </a>
-                <a href="#projects" title="Projects" >
-                    <BsBriefcaseFill/>
-                </a>
-                <a href="#contact" title="Contact" >
-                    <AiFillMail/>
-                </a>
-            </div>
-        </nav>
-    )
-}
+          Home
+        </a>
+        <a
+          className="text-[4rem] font-[260] text-black hover:underline"
+          href="#about"
+          title="About"
+        >
+          About
+        </a>
+        <a
+          className="text-[4rem] font-[260] text-black hover:underline"
+          href="#technologies"
+          title="Technologies"
+        >
+          Technologies
+        </a>
+        <a
+          className="text-[4rem] font-[260] text-black hover:underline"
+          href="#projects"
+          title="Projects"
+        >
+          Projects
+        </a>
+        <a
+          className="text-[4rem] font-[260] text-black hover:underline"
+          href="#linkedin"
+          title="Linkedin"
+        >
+          Linkedin
+        </a>
+        <a
+          className="text-[4rem] font-[260] text-black hover:underline"
+          href="#github"
+          title="Github"
+        >
+          Github
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: "#E0CE2E",
+        normalBlue: "#0556A1",
+      },
+    },
   },
   plugins: [],
-}
-
+};h
