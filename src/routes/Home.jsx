8ead64beb15920a3e420ef4c52341d14b33b1cab
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-03 17:51:10 -0300

    next.js installed

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,32 +1,19 @@
 import "../index.css";
 import "./../styles/App.scss";
-import { useState, useEffect } from "react";
-import draw from "../assets/images/draw.jpg";
-import cartoon from "../assets/images/cartoon.jpg";
-import logo from "../assets/images/logo_official.png";
+// import { useState, useEffect } from "react";
+// import draw from "../assets/images/draw.jpg";
+// import cartoon from "../assets/images/cartoon.jpg";
+// import logo from "../assets/images/logo_official.png";
 import { AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import Projects from "../sections/Projects";
-import About from "../sections/About";
-import Contact from "../sections/Contact";
-import Footer from "../components/Footer";
-import { Main } from "../sections/Main";
+// import { Link } from "react-router-dom";
+// import Projects from "../sections/Projects";
+// import About from "../sections/About";
+// import Contact from "../sections/Contact";
+// import Footer from "../components/Footer";
+// import { Main } from "../sections/Main";
 
 const Home = () => {
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Define o breakpoint para "md" como 768 pixels
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
     <div className="main gridContainer">
       <div className="section">
