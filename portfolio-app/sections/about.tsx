a3d823018ba51b9ed9ee86df579726105da3b57e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-05 14:59:05 -0300

    constants/technologies.tsx created

--- portfolio-app/sections/about.tsx
+++ portfolio-app/sections/about.tsx
@@ -1,80 +1,6 @@
 import Image from "next/image";
 import profileImg from "../assets/profile-thumb.png";
-import { FaNodeJs } from "react-icons/fa";
-import {
-  SiHtml5,
-  SiCss3,
-  SiJavascript,
-  SiTypescript,
-  SiReact,
-  SiNextdotjs,
-  SiTailwindcss,
-  SiBootstrap,
-  SiJest,
-  SiSass,
-  SiExpress,
-  SiMysql,
-  SiMongodb,
-} from "react-icons/si";
-
-const technologies = [
-  {
-    name: "HTML",
-    symbol: <SiHtml5 className="text-orange-500" />,
-  },
-  {
-    name: "CSS",
-    symbol: <SiCss3 className="text-blue-500" />,
-  },
-  {
-    name: "JavaScript",
-    symbol: <SiJavascript className="text-yellow-300" />,
-  },
-  {
-    name: "TypeScript",
-    symbol: <SiTypescript className="text-blue-500" />,
-  },
-  {
-    name: "Node.js",
-    symbol: <FaNodeJs className="text-green-400" />,
-  },
-  {
-    name: "Express",
-    symbol: <SiExpress className="text-yellow-300" />,
-  },
-  {
-    name: "Jest",
-    symbol: <SiJest className="text-red-700" />,
-  },
-  {
-    name: "React",
-    symbol: <SiReact className="text-blue-500" />,
-  },
-  {
-    name: "Next.js",
-    symbol: <SiNextdotjs className="text-white" />,
-  },
-  {
-    name: "TailwindCSS",
-    symbol: <SiTailwindcss className="text-indigo-600" />,
-  },
-  {
-    name: "Bootstrap",
-    symbol: <SiBootstrap className="text-indigo-400" />,
-  },
-  {
-    name: "SASS",
-    symbol: <SiSass className="text-pink-600" />,
-  },
-  {
-    name: "MySQL",
-    symbol: <SiMysql className="text-blue-400" />,
-  },
-  {
-    name: "MongoDB",
-    symbol: <SiSass className="text-green-600" />,
-  },
-];
+import { technologies } from "../constants/technologies";
 
 export default function About() {
   return (
@@ -115,7 +41,7 @@ export default function About() {
 
             <ul className="technologies-list">
               {technologies.map((item) => (
-                <li key={item.name} title={item.name}   >
+                <li key={item.name} title={item.name}>
                   {item.symbol}
                 </li>
               ))}
