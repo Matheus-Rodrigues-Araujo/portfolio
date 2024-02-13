c69b303398608a32b6f0aed2d8c40ecad9d28b7e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-13 02:07:15 -0300

    SEO updated

--- portfolio-app/src/app/layout.js
+++ portfolio-app/src/app/layout.js
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
@@ -16,33 +15,41 @@ import Footer from "../../components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   authors: [{ name: "Matheus Rodrigues Araujo" }],
   title: "Matheus Rodrigues Araujo | Front-end Developer & JavaScript Expert",
   description:
     "Welcome to my portfolio! I'm Matheus Rodrigues Araujo, a skilled front-end developer proficient in JavaScript, TypeScript, React, Next.js, and more. Explore my projects.",
   keywords:
     "Front-End Developer, JavaScript, React, TypeScript, Next.js, Tailwind, SQL, NoSQL, MongoDB, MySQL, Node.js, Express.js, Bootstrap, Jest, Web Development",
+  ogImage: "/logo.svg",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta charset="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.ogImage} />
         <link rel="icon" href="/logo.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
-          rel="stylesheet"
-        ></link>
+          rel="preload"
+          as="style"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
+          rel="preload"
+          as="style"
+        />
+      </Head>
       <body className={inter.className}>
         <div
           className="gap-40 md:gap-60 lg:pt-0 grid grid-flow-row md:auto-rows-[auto_1fr_1fr_1fr_1fr_auto]"
