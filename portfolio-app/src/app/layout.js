1db40bb0429c7ea357d5f20b1c79a03ceaceb0f6
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-13 02:27:50 -0300

    icon updated

--- portfolio-app/src/app/layout.js
+++ portfolio-app/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
-import logo from "./logo.svg"
+// import Head from "next/head";
+// import logo from "./logo.svg"
 
 import "../../styles/header.css";
 import "../../styles/footer.css";
@@ -23,7 +23,7 @@ export const metadata = {
     "Welcome to my portfolio! I'm Matheus Rodrigues Araujo, a skilled front-end developer proficient in JavaScript, TypeScript, React, Next.js, and more. Explore my projects.",
   keywords:
     "Front-End Developer, JavaScript, React, TypeScript, Next.js, Tailwind, SQL, NoSQL, MongoDB, MySQL, Node.js, Express.js, Bootstrap, Jest, Web Development",
-  ogImage: logo,
+  ogImage: "/favicon.ico",
 };
 
 export default function RootLayout({ children }) {
@@ -38,8 +38,8 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.ogImage} />
-        <link rel="icon" href={metadata.ogImage} />
+        {/* <meta property="og:image" content={metadata.ogImage} /> */}
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
           rel="preload"
