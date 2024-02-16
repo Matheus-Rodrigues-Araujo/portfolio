b88a158170b528d3ee279913d42e98cdbe922d59
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-16 18:57:50 -0300

    updated

--- portfolio-app/src/app/layout.tsx
+++ portfolio-app/src/app/layout.tsx
@@ -93,15 +93,14 @@ export const metadata: Metadata = {
     "Jest",
     "Web Development",
   ],
-  // robots: {
-  //   index: true,
-  //   follow: true,
-  //   nocache: true,
-  //   googleBot: {
-  //     index: true,
-  //     follow: true,
-  //   },
-  // },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
   metadataBase: new URL("https://matheuswebcoder.vercel.app"),
   category: "technology",
   openGraph: {
