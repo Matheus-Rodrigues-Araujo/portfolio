596d60a7650c8366288e271b9b07cbd63f2d6134
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-02-18 17:41:44 -0300

    next.config.mjs updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+          headers.push({
+            headers: [
+              {
+                key: 'X-Robots-Tag',
+                value: 'index',
+              },
+            ],
+            source: '/:path*',
+          });
+        }
+        return headers;
+      },
 };
 
+
 export default nextConfig;
+
