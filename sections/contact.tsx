ea134adfda0e6da4a2f4e08f567ae931c585e70e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-03 18:08:12 -0300

    h2 position of projects and contact sections has been moved

--- sections/contact.tsx
+++ sections/contact.tsx
@@ -6,22 +6,23 @@ import DynamicContactLinks from "../components/dynamicContactLinks";
 export default async function Contact() {
   return (
     <section className="contact bg-dark py-20" id="contact">
-      <h2 className="text-[10rem] md:text-[12rem] font-bold">Contact</h2>
-
-      <div className="contact-container grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-0">
-        <div className="flex justify-between flex-col">
-          <div className="flex flex-col gap-5">
-            <h3 className="text-[7rem] text-purple font-200 ">
-              Let's grap a coffee!
-            </h3>
-            <p className="text-[4.5rem] md:text-[5rem] text-light-gray w-50">
-              The time has come to transform your ideas into concrete actions!
-            </p>
-            <DynamicContactLinks />
+      <div>
+        <h2 className="text-[10rem] md:text-[12rem] font-bold">Contact</h2>
+        <div className="contact-container grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-0">
+          <div className="flex justify-between flex-col">
+            <div className="flex flex-col gap-5">
+              <h3 className="text-[7rem] text-purple font-200 ">
+                Let's grap a coffee!
+              </h3>
+              <p className="text-[4.5rem] md:text-[5rem] text-light-gray w-50">
+                The time has come to transform your ideas into concrete actions!
+              </p>
+              <DynamicContactLinks />
+            </div>
+            <DynamicGif />
           </div>
-          <DynamicGif />
+          <ContactForm />
         </div>
-        <ContactForm />
       </div>
     </section>
   );
--- sections/projects.tsx
+++ sections/projects.tsx
@@ -6,9 +6,11 @@ export default async function Projects() {
   return (
     <section className="relative bg-dark projects py-20 md:py-0 " id="projects">
       <div className="description-container">
-        <h2 className="text-[10rem] md:text-[12rem] font-bold">Projects</h2>
-        <div className="grid gap-40 mt-40">
-          <ProjectCardList projectsData={projectsData} />
+        <div>
+          <h2 className="text-[10rem] md:text-[12rem] font-bold">Projects</h2>
+          <div className="grid gap-40 mt-40">
+            <ProjectCardList projectsData={projectsData} />
+          </div>
         </div>
       </div>
     </section>
